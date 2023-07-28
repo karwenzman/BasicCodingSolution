b5e0dc06f047d1d36d026c68a7dbe1a97b13d1a9
karwenzman <jenning.thorsten@gmx.net>
2023-07-28 10:23:00 +0200

    preparing code for next issue

--- BasicCodingConsole/Program.cs
+++ BasicCodingConsole/Program.cs
@@ -2,7 +2,6 @@
 using BasicCodingConsole.Views.SettingView;
 using BasicCodingLibrary.Models;
 using BasicCodingLibrary.Providers;
-using BasicCodingLibrary.ViewModels;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,14 +27,11 @@ using var host = Host.CreateDefaultBuilder()
         services.Configure<UserInformation>(builder.Build().GetSection("UserInformation"));
         services.Configure<ApplicationInformation>(builder.Build().GetSection("ApplicationInformation"));
         services.AddTransient<IMainView, MainView>();
-        services.AddTransient<IMainViewModel, MainViewModel>();
         services.AddTransient<ISettingView, SettingView>();
-        services.AddTransient<ISettingViewModel, SettingViewModel>();
         services.AddTransient<IAppSettingProvider, AppSettingProvider>();
     })
     .UseSerilog()
     .Build();
-
 
 using var scope = host.Services.CreateScope();
 var services = scope.ServiceProvider;
