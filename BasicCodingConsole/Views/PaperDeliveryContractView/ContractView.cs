f5af4e88a26c7f93c9befbabb43d9631d6df7ac8
karwenzman <jenning.thorsten@gmx.net>
2023-09-01 11:11:17 +0200

    #13 implementing delete logic

--- BasicCodingConsole/Views/PaperDeliveryContractView/ContractView.cs
+++ BasicCodingConsole/Views/PaperDeliveryContractView/ContractView.cs
@@ -57,7 +57,7 @@ public class ContractView : ViewBase, IPaperDeliveryContractView
                         Message.Continue();
                         break;
                     case ConsoleKey.D:
-                        Console.WriteLine("No content, yet");
+                        KeystrokeD();
                         Message.Continue();
                         break;
                     case ConsoleKey.E:
@@ -241,7 +241,6 @@ public class ContractView : ViewBase, IPaperDeliveryContractView
     /// </summary>
     private void KeystrokeA()
     {
-        // contract erstellen...
         Contract = new();
         bool isValidated;
 
@@ -330,10 +329,115 @@ public class ContractView : ViewBase, IPaperDeliveryContractView
     }
 
     /// <summary>
+    /// This method is performing the logic when calling menu key 'D'.
+    /// </summary>
+    private void KeystrokeD()
+    {
+        Contract = new();
+        bool isValidated;
+        bool isRecordFound;
+        string searchId = "";
+
+        Console.WriteLine("Deleting a contract");
+        Console.WriteLine("===================");
+        Console.WriteLine("ContractID - enter date of contract (yyyymmdd): ");
+        isValidated = false;
+        do
+        {
+            var input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (input.Length == 8)
+                {
+                    searchId = input + "KA";
+                    isValidated = true;
+                    continue;
+                }
+            }
+            Console.WriteLine("Incorrect Value!");
+        } while (!isValidated);
+
+
+        // load contracts and filter contract
+        isRecordFound = false;
+        try
+        {
+            Contracts = _paperDeliveryProvider.ReadRecordsFromFile<PaperDeliveryContract>(Path.Combine(Directory.GetCurrentDirectory(), PaperDeliverySetting.PaperDeliveryDirectory, PaperDeliverySetting.ContractFile));
+            Contracts.Sort();
+            foreach (var item in Contracts)
+            {
+                if (item.Id == searchId)
+                {
+                    Contract = item;
+                    isRecordFound = true;
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError("Exception while running {method}: {error}", nameof(KeystrokeD), e);
+            Console.WriteLine($"Exception while running {nameof(KeystrokeD)}! Refer to log file for details.");
+        }
+
+        // display contract
+        if (isRecordFound)
+        {
+            Console.WriteLine(Contract.ToConsole());
+        }
+        else
+        {
+            Console.WriteLine("Record not found!");
+            return;
+        }
+
+        // confirm delete
+        Console.WriteLine("Do you want to delete this contract? (y/n)");
+        isValidated = false;
+        do
+        {
+            var input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                if (input.ToLower() == "y")
+                {
+                    Console.WriteLine("Deleting contract from contracts and save!");
+                    try
+                    {
+                        Contracts.Remove(Contract);
+                        _paperDeliveryProvider.WriteRecordsToFile<PaperDeliveryContract>(Path.Combine(Directory.GetCurrentDirectory(), PaperDeliverySetting.PaperDeliveryDirectory, PaperDeliverySetting.ContractFile), Contracts);
+                        isValidated = true;
+                        continue;
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError("Exception while running {method}: {error}", nameof(KeystrokeD), e);
+                        Console.WriteLine($"Exception while running {nameof(KeystrokeD)}! Refer to log file for details.");
+                        isValidated = true;
+                        continue;
+                    }
+                }
+                else if (input.ToLower() == "n")
+                {
+                    Console.WriteLine("Cancelling the process!");
+                    isValidated = true;
+                    continue;
+                }
+                Console.WriteLine("Incorrect Value!");
+            }
+        } while (!isValidated);
+    }
+
+    /// <summary>
     /// This method is performing the logic when calling menu key 'L'.
     /// </summary>
     private void KeystrokeL()
     {
+        Console.WriteLine("Listing contracts (sorted by Contract-ID)");
+        Console.WriteLine("=========================================");
+
         try
         {
             Contracts = _paperDeliveryProvider.ReadRecordsFromFile<PaperDeliveryContract>(Path.Combine(Directory.GetCurrentDirectory(), PaperDeliverySetting.PaperDeliveryDirectory, PaperDeliverySetting.ContractFile));
