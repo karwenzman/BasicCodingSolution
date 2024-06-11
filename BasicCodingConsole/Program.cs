b3481b8ae6d3be0e05c368ab0a3c4ad13bb4cb52
karwenzman <jenning.thorsten@gmx.net>
2024-06-11 14:51:11 +0200

    #18 Tweak
    Removes a ReadLine() command.

--- BasicCodingConsole/Program.cs
+++ BasicCodingConsole/Program.cs
@@ -29,63 +29,62 @@ Console.OutputEncoding = Encoding.Default; // this is needed to enable the conso
 Console.OutputEncoding = Encoding.UTF8; // this is needed to enable the console to print "€" symbol
 
 Log.Logger = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .WriteTo.File("LogFiles/apploggings.txt")
-    .CreateLogger();
+	.Enrich.FromLogContext()
+	.WriteTo.File("LogFiles/apploggings.txt")
+	.CreateLogger();
 
 using var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((context, services) =>
-    {
-        services.AddLogging();
-        services.AddOptions<AppSetting>().Bind(context.Configuration.GetSection(nameof(AppSetting)));
-        services.AddOptions<ConnectionStrings>().Bind(context.Configuration.GetSection(nameof(ConnectionStrings)));
-        services.AddOptions<ApplicationSetting>().Bind(context.Configuration.GetSection(nameof(ApplicationSetting)));
-        services.AddOptions<PaperDeliverySetting>().Bind(context.Configuration.GetSection(nameof(PaperDeliverySetting)));
-        services.AddOptions<ConsoleSetting>().Bind(context.Configuration.GetSection(nameof(ConsoleSetting)));
-        services.AddOptions<UserSetting>().Bind(context.Configuration.GetSection(nameof(UserSetting)));
-        services.AddTransient<IStartup, Startup>();
-        services.AddTransient<IMainView, MainView>();
-        services.AddTransient<ISettingView, SettingView>();
-        services.AddTransient<IPaperDeliveryView, PaperDeliveryView>();
-        services.AddTransient<IPaperDeliveryReferenceDataView, PaperDeliveryReferenceDataView>();
-        services.AddTransient<IPaperDeliveryClientView, PaperDeliveryClientView>();
-        services.AddTransient<IPaperDeliveryContractView, PaperDeliveryContractView>();
-        services.AddTransient<IPaperDeliveryContractorView, PaperDeliveryContractorView>();
-        services.AddTransient<IPaperDeliveryOperationalDataView, PaperDeliveryOperationalDataView>();
-        services.AddTransient<IPaperDeliveryProvider, PaperDeliveryProvider>();
-    })
-    .UseSerilog()
-    .Build();
+	.ConfigureServices((context, services) =>
+	{
+		services.AddLogging();
+		services.AddOptions<AppSetting>().Bind(context.Configuration.GetSection(nameof(AppSetting)));
+		services.AddOptions<ConnectionStrings>().Bind(context.Configuration.GetSection(nameof(ConnectionStrings)));
+		services.AddOptions<ApplicationSetting>().Bind(context.Configuration.GetSection(nameof(ApplicationSetting)));
+		services.AddOptions<PaperDeliverySetting>().Bind(context.Configuration.GetSection(nameof(PaperDeliverySetting)));
+		services.AddOptions<ConsoleSetting>().Bind(context.Configuration.GetSection(nameof(ConsoleSetting)));
+		services.AddOptions<UserSetting>().Bind(context.Configuration.GetSection(nameof(UserSetting)));
+		services.AddTransient<IStartup, Startup>();
+		services.AddTransient<IMainView, MainView>();
+		services.AddTransient<ISettingView, SettingView>();
+		services.AddTransient<IPaperDeliveryView, PaperDeliveryView>();
+		services.AddTransient<IPaperDeliveryReferenceDataView, PaperDeliveryReferenceDataView>();
+		services.AddTransient<IPaperDeliveryClientView, PaperDeliveryClientView>();
+		services.AddTransient<IPaperDeliveryContractView, PaperDeliveryContractView>();
+		services.AddTransient<IPaperDeliveryContractorView, PaperDeliveryContractorView>();
+		services.AddTransient<IPaperDeliveryOperationalDataView, PaperDeliveryOperationalDataView>();
+		services.AddTransient<IPaperDeliveryProvider, PaperDeliveryProvider>();
+	})
+	.UseSerilog()
+	.Build();
 
 using var scope = host.Services.CreateScope();
 var services = scope.ServiceProvider;
 
 try
 {
-    Log.Logger.Information("***** Run Application *****");
-    services.GetRequiredService<IStartup>().WriteSettingsToConsole();
-    #region ====> for testing IOptionsSnapshot<> when calling GetRequiredService<> a second time
-    // without IOptionsSnapshot, the changed data will not be loaded
-    // this is leading to the question, how to call my services properly
-    // only GetRequiredService<> is calling the constructor and loading the options
-    // calling Run() will use the injected data from constructor
-    // if both Startup and ContractView are using IOptionsSnapshot, then the changed data will not be loaded ?????
-    Console.ReadLine();
-    #endregion
-    services.GetRequiredService<IStartup>().Run();
+	Log.Logger.Information("***** Run Application *****");
+	services.GetRequiredService<IStartup>().WriteSettingsToConsole();
+	#region ====> for testing IOptionsSnapshot<> when calling GetRequiredService<> a second time
+	// without IOptionsSnapshot, the changed data will not be loaded
+	// this is leading to the question, how to call my services properly
+	// only GetRequiredService<> is calling the constructor and loading the options
+	// calling Run() will use the injected data from constructor
+	// if both Startup and ContractView are using IOptionsSnapshot, then the changed data will not be loaded ?????
+	#endregion
+	services.GetRequiredService<IStartup>().Run();
 }
 catch (Exception e)
 {
-    Log.Logger.Error("Unexpected Exception: {error}", e);
+	Log.Logger.Error("Unexpected Exception: {error}", e);
 
-    Console.WriteLine("Unexpected Exception!");
-    Console.WriteLine(e);
-    Console.WriteLine($"\n***** Press ENTER To Continue *****");
-    Console.ReadLine();
+	Console.WriteLine("Unexpected Exception!");
+	Console.WriteLine(e);
+	Console.WriteLine($"\n***** Press ENTER To Continue *****");
+	Console.ReadLine();
 }
 finally
 {
-    Log.Logger.Information("***** End Application *****");
+	Log.Logger.Information("***** End Application *****");
 }
 
 #if DEBUG
